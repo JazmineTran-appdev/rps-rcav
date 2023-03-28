2ea3bacfa8f7f4c175dc2f94965fcdc7ab9f1390
Jazmine Tran <jazminetra00@gmail.com>
2023-03-28 16:08:41 +0000

    added rock route (test)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+    def play_rock
+      # write your code here
+
+      redirect_to("https://wikipedia.org")
+    end
 end
